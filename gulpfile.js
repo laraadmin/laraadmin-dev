bdababc53b7fffcfb5a5336e961d70b9679049b6
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-06-01 19:07:37 +0530

    datatables updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,19 @@
 var elixir = require('laravel-elixir');
+/*
+var minify = require('gulp-minify');
+gulp.task('compress', function() {
+  gulp.src('lib/*.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest('dist'))
+});
+*/
 
 /*
  |--------------------------------------------------------------------------
--- resources/views/layouts/partials/contentheader.blade.php
+++ resources/views/layouts/partials/contentheader.blade.php
@@ -4,8 +4,14 @@
         @yield('contentheader_title', 'Page Header here')
         <small>@yield('contentheader_description')</small>
     </h1>
+    @hasSection('headerElems')
+        <span class="headerElems">
+        @yield('headerElems')
+        </span>
+    @else hasSection('section')
     <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
+        <li><a href="#"><i class="fa fa-dashboard"></i> @hasSection('section') @yield('section') @else Level @endif</a></li>
+        <li class="active">@hasSection('sub_section') @yield('sub_section') @else Here @endif</li>
     </ol>
+    @endif
 </section>
