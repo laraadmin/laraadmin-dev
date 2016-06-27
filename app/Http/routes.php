4e585ad4d83756fdce7dbaa1dcb199feadd636a2
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-06-27 22:55:37 +0530

    Changes from new LA version

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,14 @@
 |
 */
 
+/* ================== Homepage ================== */
+
 Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index');
 
 Route::auth();
+
+/* ================== Dashboard ================== */
 
 Route::get(config('laraadmin.adminRoute'), 'LA\DashboardController@index');
 Route::get(config('laraadmin.adminRoute'). '/dashboard', 'LA\DashboardController@index');
@@ -23,10 +27,10 @@ Route::get('/dashboard', 'LA\DashboardController@index');
 /* ================== Users ================== */
 Route::get(config('laraadmin.adminRoute') . '/user/{id}', 'LA\UserController@show');
 
-/* ================== Books ================== */
-Route::resource(config('laraadmin.adminRoute') . '/books', 'LA\BooksController');
-Route::get(config('laraadmin.adminRoute') . '/book_dt_ajax', 'LA\BooksController@dtajax');
-
 /* ================== Employees ================== */
 Route::resource(config('laraadmin.adminRoute') . '/employees', 'LA\EmployeesController');
 Route::get(config('laraadmin.adminRoute') . '/employee_dt_ajax', 'LA\EmployeesController@dtajax');
+
+/* ================== Books ================== */
+Route::resource(config('laraadmin.adminRoute') . '/books', 'LA\BooksController');
+Route::get(config('laraadmin.adminRoute') . '/book_dt_ajax', 'LA\BooksController@dtajax');
