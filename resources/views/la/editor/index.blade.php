02d9c72c443e6a336d040cde959e9f1757c892d7
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-06-28 19:33:53 +0530

    saving file done

--- resources/views/la/editor/index.blade.php
+++ resources/views/la/editor/index.blade.php
@@ -4,10 +4,9 @@
 	Code Editor
 @endsection
 
-
 @section('main-content')
 <div id="laeditor" class="row">
-	<div class="col-md-2">
+	<div class="col-md-2 col-sm-3">
 		<div class="la-header">
 			LA Editor
 			<!--<div class="la-dir">/Applications/MAMP/htdocs</div>-->
@@ -16,7 +15,7 @@
 			
 		</div>
 	</div>
-	<div class="col-md-10">
+	<div class="col-md-10 col-sm-9">
 		<ul class="laeditor-tabs">
 			
 		</ul>
@@ -37,6 +36,9 @@
 <script>
 var $openFiles = [];
 var laeditor = null;
+var cntFile;
+var modelist = ace.require("ace/ext/modelist");
+
 $(function () {
 	// Start Jquery File Tree
 	$('.la-file-tree').fileTree({
@@ -53,6 +55,14 @@ $(function () {
     laeditor.setTheme("ace/theme/twilight");
     laeditor.session.setMode("ace/mode/javascript");
 	laeditor.$blockScrolling = Infinity
+	laeditor.commands.addCommand({
+		name: 'save',
+		bindKey: {win: "Ctrl-S", "mac": "Cmd-S"},
+		exec: function(editor) {
+			// console.log("saving", editor.session.getValue());
+			saveFileCode(cntFile, editor.session.getValue());
+		}
+	})
 	
 	setEditorSize();
 	
@@ -73,68 +83,122 @@ function setEditorSize() {
 	$("#la-ace-editor").css("max-height", editorHeight+"px");
 }
 $laetabs = $(".laeditor-tabs");
+
+$(".laeditor-tabs").on("click", "li i.fa", function(e) {
+	filepath = $(this).parent().attr("filepath");
+	closeFile(filepath);
+	e.stopPropagation();
+});
+$(".laeditor-tabs").on("click", "li", function(e) {
+	filepath = $(this).attr("filepath");
+	openFile(filepath);
+	e.stopPropagation();
+});
+
 function openFile(filepath) {
-	console.log("openFile: "+filepath);
-	if(!contains($openFiles, filepath)) {
-		$openFiles.push(filepath);
-		var filename = filepath.replace(/^.*[\\\/]/, '');
-		$laetabs.append('<li filepath="'+filepath+'">'+filename+' <i class="fa fa-5x fa-times"></i></li>');
-	} else {
-		
-	}
-	highlightFileTab(filepath);
+	var fileFound = fileContains(filepath);
+	console.log("openFile: "+filepath+" fileFound: "+fileFound);
+	
 	loadFileCode(filepath);
 	console.log($openFiles);
 }
 
-function highlightFileTab(filepath) {
-	$laetabs.children("li").removeClass("active");
-	$laetabs.children("li[filepath='"+filepath+"']").addClass("active");
+function closeFile(filepath) {
+	console.log("closeFile: "+filepath);
+	// $openFiles[getFileIndex(filepath)] = null;
+	var index = getFileIndex(filepath);
+	console.log("index: "+index);
+	$openFiles.splice(index, 1);
+	$laetabs.children("li[filepath='"+filepath+"']").remove();
+	console.log($openFiles);
+	
+	if(index != 0 && $openFiles.length != 0) {
+		openFile($openFiles[index-1].filepath);
+	} else {
+		laeditor.setValue("", -1);
+		laeditor.focus();
+		laeditor.session.setMode("ace/mode/text");
+	}
 }
 
-function loadFileCode(filepath) {
+function loadFileCode(filepath, reload = false) {
+	console.log("loadFileCode: "+filepath+" contains: "+fileContains(filepath));
+	if(!fileContains(filepath)) {
+		$.ajax({
+			url: "{{ url(config('laraadmin.adminRoute') . '/laeditor_get_file?_token=' . csrf_token()) }}",
+			method: 'POST',
+			data: {"filepath": filepath},
+			async: false,
+			success: function( data ) {
+				//console.log(data);
+				laeditor.setValue(data, -1);
+				laeditor.focus();
+				
+				var mode = modelist.getModeForPath(filepath).mode;
+				laeditor.session.setMode(mode);
+				
+				// $openFiles[getFileIndex(filepath)].filedata = data;
+				// $openFiles[getFileIndex(filepath)].filemode = mode;
+				
+				$file = {
+					"filepath": filepath,
+					"filedata": data,
+					"filemode": mode
+				}
+				$openFiles.push($file);
+				var filename = filepath.replace(/^.*[\\\/]/, '');
+				$laetabs.append('<li filepath="'+filepath+'">'+filename+' <i class="fa fa-5x fa-times"></i></li>');
+				highlightFileTab(filepath);
+			}
+		});
+	} else {
+		console.log("File found offline");
+		var data = $openFiles[getFileIndex(filepath)].filedata;
+		laeditor.setValue(data, -1);
+		laeditor.focus();
+		var mode = modelist.getModeForPath(filepath).mode;
+		laeditor.session.setMode(mode);
+		highlightFileTab(filepath);
+	}
+}
+
+function saveFileCode(filepath, filedata, reload = false) {
 	$.ajax({
-		url: "{{ url(config('laraadmin.adminRoute') . '/laeditor_get_file?_token=' . csrf_token()) }}",
+		url: "{{ url(config('laraadmin.adminRoute') . '/laeditor_save_file?_token=' . csrf_token()) }}",
 		method: 'POST',
-		data: {filepath: filepath},
+		data: {
+			"filepath": filepath,
+			"filedata": filedata
+		},
+		async: false,
 		success: function( data ) {
-			//console.log(data);
-			laeditor.setValue(data);
-			
-			
-			var modelist = ace.require("ace/ext/modelist");
-			var mode = modelist.getModeForPath(filepath).mode;
-			laeditor.session.setMode(mode); // mode now contains "ace/mode/javascript".
-			
-			// var ext = filepath.split('.').pop();
-			// console.log(ext);
-			// switch(ext) {
-			// 	case "css":
-			// 	case "less":
-			// 	case "sass":
-			// 		laeditor.getSession().setMode("ace/mode/css");
-			// 		break;
-			// 	case "json":
-			// 	case "js":
-			// 		laeditor.getSession().setMode("ace/mode/javascript");
-			// 		break;
-			// 	case "php":
-			// 		laeditor.getSession().setMode("ace/mode/php");
-			// 		break;
-			// 	default:
-			// 		laeditor.getSession().setMode("ace/mode/text");
-			// }
+			console.log(data);
 		}
 	});
 }
 
-function contains(a, obj) {
-    for (var i = 0; i < a.length; i++) {
-        if (a[i] === obj) {
-            return true;
-        }
-    }
-    return false;
+function highlightFileTab(filepath) {
+	cntFile = filepath;
+	$laetabs.children("li").removeClass("active");
+	$laetabs.children("li[filepath='"+filepath+"']").addClass("active");
 }
+
+function getFileIndex(filepath) {
+	for (var i=0; i < $openFiles.length; i++) {
+		if($openFiles[i].filepath == filepath) {
+			return i;
+		}
+	}
+}
+
+function fileContains(filepath) {
+	for (var i=0; i < $openFiles.length; i++) {
+		if($openFiles[i].filepath == filepath) {
+			return true;
+		}
+	}
+	return false;
+}
+
 </script>
 @endpush
