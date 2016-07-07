402a03d00b8212aff2ddbe154bf334694356aaed
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-07-07 13:55:31 +0530

    Menu Editor added - Bugs

--- resources/views/la/menus/index.blade.php
+++ resources/views/la/menus/index.blade.php
@@ -16,88 +16,71 @@ use Dwij\Laraadmin\Models\Module;
 
 @section("main-content")
 
-<style>
-.dd { position: relative; display: block; margin: 0; padding: 0; max-width: 600px; list-style: none; font-size: 13px; line-height: 20px; }
-
-.dd-list { display: block; position: relative; margin: 0; padding: 0; list-style: none; }
-.dd-list .dd-list { padding-left: 30px; }
-.dd-collapsed .dd-list { display: none; }
-
-.dd-item,
-.dd-empty,
-.dd-placeholder { display: block; position: relative; margin: 0; padding: 0; min-height: 20px; font-size: 13px; line-height: 20px; }
-
-.dd-handle { display: block; height: 30px; max-width: 200px;margin: 5px 0; padding: 5px 10px; color: #333; text-decoration: none; font-weight: bold; border: 1px solid #ccc;
-    background: #fafafa;
-    background: -webkit-linear-gradient(top, #fafafa 0%, #eee 100%);
-    background:    -moz-linear-gradient(top, #fafafa 0%, #eee 100%);
-    background:         linear-gradient(top, #fafafa 0%, #eee 100%);
-    -webkit-border-radius: 3px;
-            border-radius: 3px;
-    box-sizing: border-box; -moz-box-sizing: border-box;
-}
-.dd-handle:hover { color: #2ea8e5; background: #fff; }
-
-.dd-item > button { display: block; position: relative; cursor: pointer; float: left; width: 25px; height: 20px; margin: 5px 0; padding: 0; text-indent: 100%; white-space: nowrap; overflow: hidden; border: 0; background: transparent; font-size: 12px; line-height: 1; text-align: center; font-weight: bold; }
-.dd-item > button:before { content: '+'; display: block; position: absolute; width: 100%; text-align: center; text-indent: 0; }
-.dd-item > button[data-action="collapse"]:before { content: '-'; }
-
-.dd-placeholder,
-.dd-empty { margin: 5px 0; padding: 0; min-height: 30px; background: #f2fbff; border: 1px dashed #b6bcbf; box-sizing: border-box; -moz-box-sizing: border-box; }
-.dd-empty { border: 1px dashed #bbb; min-height: 100px; background-color: #e5e5e5;
-    background-image: -webkit-linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff),
-                      -webkit-linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff);
-    background-image:    -moz-linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff),
-                         -moz-linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff);
-    background-image:         linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff),
-                              linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%, #fff);
-    background-size: 60px 60px;
-    background-position: 0 0, 30px 30px;
-}
-
-.dd-dragel { position: absolute; pointer-events: none; z-index: 9999; }
-.dd-dragel > .dd-item .dd-handle { margin-top: 0; }
-.dd-dragel .dd-handle {
-    -webkit-box-shadow: 2px 4px 6px 0 rgba(0,0,0,.1);
-            box-shadow: 2px 4px 6px 0 rgba(0,0,0,.1);
-}
-</style>
-
-<div class="box box-success">
+<div class="box box-success menus">
 	<!--<div class="box-header"></div>-->
 	<div class="box-body">
 		<div class="row">
 			<div class="col-md-4 col-lg-4">
-
+				<div class="nav-tabs-custom">
+					<ul class="nav nav-tabs">
+						<li class="active"><a href="#tab-modules" data-toggle="tab">Modules</a></li>
+						<li><a href="#tab-custom-link" data-toggle="tab">Custom Links</a></li>
+					</ul>
+					<div class="tab-content">
+						<div class="tab-pane active" id="tab-modules">
+							<ul>
+							@foreach ($modules as $module)
+								<li><i class="fa {{ $module->fa_icon }}"></i> {{ $module->name }} <a class="pull-right"><i class="fa fa-plus"></i></a></li>
+							@endforeach
+							</ul>
+						</div>
+						<div class="tab-pane" id="tab-custom-link">
+							{!! Form::open(['id' => 'menu-custom-form']) !!}
+								<div class="form-group">
+									<label for="url" style="font-weight:normal;">URL</label>
+									<input class="form-control" placeholder="URL" name="url" type="text" value="http://" data-rule-minlength="1" required>
+								</div>
+								<div class="form-group">
+									<label for="label" style="font-weight:normal;">Label</label>
+									<input class="form-control" placeholder="Label" name="label" type="text" value=""  data-rule-minlength="1" required>
+								</div>
+								<div class="form-group">
+									<label for="icon" style="font-weight:normal;">Icon</label>
+									<div class="input-group">
+										<input class="form-control" placeholder="FontAwesome Icon" name="icon" type="text" value="fa-cube"  data-rule-minlength="1" required>
+										<span class="input-group-addon"></span>
+									</div>
+								</div>
+								<input type="submit" class="btn btn-primary pull-right mr10" value="Add to menu">
+							{!! Form::close() !!}
+						</div>
+					</div><!-- /.tab-content -->
+				</div><!-- nav-tabs-custom -->
 			</div>
 			<div class="col-md-8 col-lg-8">
-				<div class="dd" id="nestable">
+				<div class="dd" id="menu-nestable">
+					{{ json_encode($menus) }}
+					{{ LAHelper::print_menu_editor($menus) }}
 					<ol class="dd-list">
-						<li class="dd-item" data-id="1">
-							<div class="dd-handle">Item 1</div>
+						<li class="dd-item dd3-item" data-id="13">
+							<div class="dd-handle dd3-handle">Drag</div><div class="dd3-content">Item 13 <button class="btn btn-xs btn-danger pull-right"><i class="fa fa-times"></i></button> <button class="btn btn-xs btn-success pull-right"><i class="fa fa-edit"></i></button></div>
 						</li>
-						<li class="dd-item" data-id="2">
-							<div class="dd-handle">Item 2</div>
+						<li class="dd-item dd3-item" data-id="14">
+							<div class="dd-handle dd3-handle">Drag</div><div class="dd3-content">Item 14</div>
+						</li>
+						<li class="dd-item dd3-item" data-id="15">
+							<div class="dd-handle dd3-handle">Drag</div><div class="dd3-content">Item 15</div>
 							<ol class="dd-list">
-								<li class="dd-item" data-id="3"><div class="dd-handle">Item 3</div></li>
-								<li class="dd-item" data-id="4"><div class="dd-handle">Item 4</div></li>
-								<li class="dd-item" data-id="5">
-									<div class="dd-handle">Item 5</div>
-									<ol class="dd-list">
-										<li class="dd-item" data-id="6"><div class="dd-handle">Item 6</div></li>
-										<li class="dd-item" data-id="7"><div class="dd-handle">Item 7</div></li>
-										<li class="dd-item" data-id="8"><div class="dd-handle">Item 8</div></li>
-									</ol>
+								<li class="dd-item dd3-item" data-id="16">
+									<div class="dd-handle dd3-handle">Drag</div><div class="dd3-content">Item 16</div>
 								</li>
-								<li class="dd-item" data-id="9"><div class="dd-handle">Item 9</div></li>
-								<li class="dd-item" data-id="10"><div class="dd-handle">Item 10</div></li>
+								<li class="dd-item dd3-item" data-id="17">
+									<div class="dd-handle dd3-handle">Drag</div><div class="dd3-content">Item 17</div>
+								</li>
+								<li class="dd-item dd3-item" data-id="18">
+									<div class="dd-handle dd3-handle">Drag</div><div class="dd3-content">Item 18</div>
+								</li>
 							</ol>
-						</li>
-						<li class="dd-item" data-id="11">
-							<div class="dd-handle">Item 11</div>
-						</li>
-						<li class="dd-item" data-id="12">
-							<div class="dd-handle">Item 12</div>
 						</li>
 					</ol>
 				</div>
@@ -110,11 +93,17 @@ use Dwij\Laraadmin\Models\Module;
 
 @push('scripts')
 <script src="{{ asset('la-assets/plugins/nestable/jquery.nestable.js') }}"></script>
+<script src="{{ asset('la-assets/plugins/iconpicker/fontawesome-iconpicker.js') }}"></script>
 <script>
 $(function () {
-	$('#nestable').nestable({
+	$('input[name=icon]').iconpicker();
+
+	$('#menu-nestable').nestable({
         group: 1
     });
+	$("#menu-custom-form").validate({
+		
+	});
 });
 </script>
 @endpush
