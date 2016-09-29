d5f52ed632e4397f860e21c390d2d599c06991a9
Sagar Koshti <sagarkoshti1990@gmail.com>
2016-09-29 17:34:03 +0530

    Routes Bug Fixes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,12 +12,9 @@
 */
 
 /* ================== Homepage ================== */
-
-Route::group(['middleware' => ['web']], function () {
-	Route::get('/', 'HomeController@index');
-	Route::get('/home', 'HomeController@index');
-	Route::auth();
-});
+Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
+Route::auth();
 
 /* ================== Access Uploaded Files ================== */
 Route::get('files/{hash}/{name}', 'LA\UploadsController@get_file');
@@ -33,6 +30,4 @@ Route::get('files/{hash}/{name}', 'LA\UploadsController@get_file');
 |
 */
 
-if(Request::is(config('laraadmin.adminRoute')) || Request::is(config('laraadmin.adminRoute').'/*')) {
-	require __DIR__.'/admin_routes.php';
-}
+require __DIR__.'/admin_routes.php';
