3a408fadb8527c7568c3cffb6e09c9882ec2f028
Ganesh Bhosale <gdb.sci123@gmail.com>
2016-10-17 14:06:48 +0530

    Updated routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,33 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-/* ================== Homepage ================== */
-Route::get('/', 'HomeController@index');
-Route::get('/home', 'HomeController@index');
-Route::auth();
-
-/* ================== Access Uploaded Files ================== */
-Route::get('files/{hash}/{name}', 'LA\UploadsController@get_file');
-
-/*
-|--------------------------------------------------------------------------
-| Admin Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+/* ================== Homepage + Admin Routes ================== */
 
 require __DIR__.'/admin_routes.php';
